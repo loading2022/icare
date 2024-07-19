6dff85784cb176b2327f7524bb9cc3543f39b8ee
loading2022 <aa0421626@gmail.com>
2024-07-19 14:19:45 +0800

    try commit on github

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import threading
 import queue
 import azure.cognitiveservices.speech as speechsdk
-from flask import Flask, render_template, request, jsonify, url_for, send_from_directory
+from flask import Flask, render_template, request, jsonify, url_for, send_from_directory, session
 import openai
 import uuid
 import requests
@@ -14,30 +14,90 @@ from deepface import DeepFace
 from PIL import ImageFont, ImageDraw, Image
 
 # 變數定義
+subscription_key = '295f2324100845f3ada64f6cb0f598e5'
 region = 'eastus2'
-img_url = "https://pmumedicaldevice.com/cdn/shop/files/lifestyle-beauty-fashion-people-emotions-concept-goodlooking-young-female-striped-shirt-point.jpg?v=1712305433&width=750"
+openai_api_key = "sk-proj-5zkksvA3JQIJojvGJ05UT3BlbkFJ7A4stZjfxMC5fM0uFv8o"
 
 app = Flask(__name__)
+app.secret_key = 'JQDxzgL0hjIHBO-xcCwGMw' 
 
+global img_url 
 # 線程安全的佇列用來存儲結果 URL
 result_queue = queue.Queue()
 #result_queue.put("https://studio.d-id.com/share?id=401404189a5049f2d9546e8a48e32fa1&utm_source=copy")
 # 設定 GPT API
-def call_gpt(text, emotion):
+def call_gpt(text, role, emotion):
     openai.api_key = openai_api_key
     print(emotion)
+    # 根據角色生成不同的 prompt
+    if role == '照護員':
+        prompt = f"你的名字是小妤，使用者為年長者，你須扮演一個陪伴年長者的角色,如果使用者有提及病痛相關的內容，請回應「好的，我會幫您向您的家人反應」，再依據 user 內容及{emotion}給予適當回應，請以較口語化的方式回應，並請勿反駁使用者所說的內容，結果請以繁體中文"
+    elif role == '平輩':
+        prompt = f"你的名字是小妤，使用者為年長者，你須扮演一個朋友或同齡人的角色，內容須表現出共感的感覺，依據 user 內容及{emotion}給予適當回應，請以較口語化的方式回應，並請勿反駁使用者所說的內容，結果請以繁體中文"
+    elif role == '晚輩':
+        prompt = f"你的名字是小妤，使用者為年長者，你須扮演一個晚輩或孫輩的角色，依據 user 內容及{emotion}給予適當回應，請以較口語化的方式回應，並請勿反駁使用者所說的內容，結果請以繁體中文"
+    
     response = openai.ChatCompletion.create(
-        model ="gpt-4o",
+        model="gpt-4o",
         messages=[
-            {"role": "system", "content": f"你的名字是小妤，使用者為年長者，你須扮演一個陪伴年長者的角色，依據 user 內容及{emotion}給予適當回應，請連貫之前所說的內容，請以較口語化的方式回應，請控制在30字以內作回答，並請勿反駁使用者所說的內容，結果請以繁體中文"},
+            {"role": "system", "content": prompt},
             {"role": "user", "content": text}
         ]
     )
     return response.choices[0].message.content
 
+# 建立 D-ID URL
+def create_did(text, img_url):
+    url = "https://api.d-id.com/talks"
+    print(f"D-ID: {img_url}")
+    payload = {
+    "script": {
+        "type": "text",
+        "subtitles": "false",
+        "provider": {
+            "type": "microsoft",
+            "voice_id": "zh-CN-XiaoxiaoNeural"
+        },
+        "ssml": "false",
+        "input": text
+    },
+    "config": {
+        "fluent": "false",
+        "pad_audio": "0.0"
+    },
+    "source_url": img_url
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik53ek53TmV1R3ptcFZTQjNVZ0J4ZyJ9.eyJodHRwczovL2QtaWQuY29tL2ZlYXR1cmVzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9pZCI6InByb2RfTXRkY2RrM29nR3hkNlkiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9jdXN0b21lcl9pZCI6ImN1c19ROXF5a0E4UThTWnR4SyIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3Byb2R1Y3RfbmFtZSI6ImxpdGUiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiJzdWJfMVBKWEdESnhFS1oyekF5blVMRldtVjF0IiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfYmlsbGluZ19pbnRlcnZhbCI6Im1vbnRoIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcGxhbl9ncm91cCI6ImRlaWQtbGl0ZSIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3ByaWNlX2lkIjoicHJpY2VfMU5TYmR6SnhFS1oyekF5bmNYN1dRVWoxIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJpY2VfY3JlZGl0cyI6IjY0IiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2NyZWRpdHMiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9wcm92aWRlciI6Imdvb2dsZS1vYXV0aDIiLCJodHRwczovL2QtaWQuY29tL2lzX25ldyI6ZmFsc2UsImh0dHBzOi8vZC1pZC5jb20vYXBpX2tleV9tb2RpZmllZF9hdCI6IjIwMjQtMDUtMjNUMDg6NTI6NDUuMTc0WiIsImh0dHBzOi8vZC1pZC5jb20vb3JnX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9hcHBzX3Zpc2l0ZWQiOlsiU3R1ZGlvIl0sImh0dHBzOi8vZC1pZC5jb20vY3hfbG9naWNfaWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NyZWF0aW9uX3RpbWVzdGFtcCI6IjIwMjMtMTEtMjNUMDg6MTM6MjEuMTQ4WiIsImh0dHBzOi8vZC1pZC5jb20vYXBpX2dhdGV3YXlfa2V5X2lkIjoiYmo4eHpxODdkMiIsImh0dHBzOi8vZC1pZC5jb20vdXNhZ2VfaWRlbnRpZmllcl9rZXkiOiJ1c2dfQUU3QXhGWERucTdzOWtFYnJHbjR4IiwiaHR0cHM6Ly9kLWlkLmNvbS9oYXNoX2tleSI6InBlZTlNOUd0OE82eFAtRi1teXNjdSIsImh0dHBzOi8vZC1pZC5jb20vcHJpbWFyeSI6dHJ1ZSwiaHR0cHM6Ly9kLWlkLmNvbS9lbWFpbCI6IjExMXl6dWltYmlsYWJAZ21haWwuY29tIiwiaHR0cHM6Ly9kLWlkLmNvbS9wYXltZW50X3Byb3ZpZGVyIjoic3RyaXBlIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmQtaWQuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA1OTI5NDcwMjAyMTkxMjc4ODk1IiwiYXVkIjpbImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzIxMTMzNTU4LCJleHAiOjE3MjEyMTk5NTgsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIgdXBkYXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBvZmZsaW5lX2FjY2VzcyIsImF6cCI6Ikd6ck5JMU9yZTlGTTNFZURSZjNtM3ozVFN3MEpsUllxIn0.VOWyKhXnejmtNywbGLVBAmVPsc2np3aQY5PkTMz1YKSbr2nXzH8ig6lJxzbuH4rtxYJ_UxasCnvRr95sIEqvxBHNhI-TroD-POm1cCCnZhFr1Ggg5OrvESBXUqyTnACzIoLyQG8AEyc544ivaTJk-z4cbn78u5aKD3b1zn1B_EkaLJ_BJdQZ9J55I2J4_JwHJe_FAHSB8Jy5lvhiNbUiDBMqisVxJd-i399U1qG6alAGqxvjiAJcq8AWJyWrCAL6kcpNDE7nQCkEMCPbSVx5jXIhZXnLbSH9JWYZsZXeQUh8PgRmUpTitRszPqg0B7Guf9mFVtpaWyXciY2BsDsgAQ"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    response_json = json.loads(response.text)
+    print(response_json)
+
+    talk_id = response_json['id']
+    url = "https://api.d-id.com/talks/"+talk_id
+    print(url)
+    headers = {
+        "accept": "application/json",
+        "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik53ek53TmV1R3ptcFZTQjNVZ0J4ZyJ9.eyJodHRwczovL2QtaWQuY29tL2ZlYXR1cmVzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9pZCI6InByb2RfTXRkY2RrM29nR3hkNlkiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9jdXN0b21lcl9pZCI6ImN1c19ROXF5a0E4UThTWnR4SyIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3Byb2R1Y3RfbmFtZSI6ImxpdGUiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiJzdWJfMVBKWEdESnhFS1oyekF5blVMRldtVjF0IiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfYmlsbGluZ19pbnRlcnZhbCI6Im1vbnRoIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcGxhbl9ncm91cCI6ImRlaWQtbGl0ZSIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3ByaWNlX2lkIjoicHJpY2VfMU5TYmR6SnhFS1oyekF5bmNYN1dRVWoxIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJpY2VfY3JlZGl0cyI6IjY0IiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2NyZWRpdHMiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9wcm92aWRlciI6Imdvb2dsZS1vYXV0aDIiLCJodHRwczovL2QtaWQuY29tL2lzX25ldyI6ZmFsc2UsImh0dHBzOi8vZC1pZC5jb20vYXBpX2tleV9tb2RpZmllZF9hdCI6IjIwMjQtMDUtMjNUMDg6NTI6NDUuMTc0WiIsImh0dHBzOi8vZC1pZC5jb20vb3JnX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9hcHBzX3Zpc2l0ZWQiOlsiU3R1ZGlvIl0sImh0dHBzOi8vZC1pZC5jb20vY3hfbG9naWNfaWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NyZWF0aW9uX3RpbWVzdGFtcCI6IjIwMjMtMTEtMjNUMDg6MTM6MjEuMTQ4WiIsImh0dHBzOi8vZC1pZC5jb20vYXBpX2dhdGV3YXlfa2V5X2lkIjoiYmo4eHpxODdkMiIsImh0dHBzOi8vZC1pZC5jb20vdXNhZ2VfaWRlbnRpZmllcl9rZXkiOiJ1c2dfQUU3QXhGWERucTdzOWtFYnJHbjR4IiwiaHR0cHM6Ly9kLWlkLmNvbS9oYXNoX2tleSI6InBlZTlNOUd0OE82eFAtRi1teXNjdSIsImh0dHBzOi8vZC1pZC5jb20vcHJpbWFyeSI6dHJ1ZSwiaHR0cHM6Ly9kLWlkLmNvbS9lbWFpbCI6IjExMXl6dWltYmlsYWJAZ21haWwuY29tIiwiaHR0cHM6Ly9kLWlkLmNvbS9wYXltZW50X3Byb3ZpZGVyIjoic3RyaXBlIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmQtaWQuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA1OTI5NDcwMjAyMTkxMjc4ODk1IiwiYXVkIjpbImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzIxMTMzNTU4LCJleHAiOjE3MjEyMTk5NTgsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIgdXBkYXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBvZmZsaW5lX2FjY2VzcyIsImF6cCI6Ikd6ck5JMU9yZTlGTTNFZURSZjNtM3ozVFN3MEpsUllxIn0.VOWyKhXnejmtNywbGLVBAmVPsc2np3aQY5PkTMz1YKSbr2nXzH8ig6lJxzbuH4rtxYJ_UxasCnvRr95sIEqvxBHNhI-TroD-POm1cCCnZhFr1Ggg5OrvESBXUqyTnACzIoLyQG8AEyc544ivaTJk-z4cbn78u5aKD3b1zn1B_EkaLJ_BJdQZ9J55I2J4_JwHJe_FAHSB8Jy5lvhiNbUiDBMqisVxJd-i399U1qG6alAGqxvjiAJcq8AWJyWrCAL6kcpNDE7nQCkEMCPbSVx5jXIhZXnLbSH9JWYZsZXeQUh8PgRmUpTitRszPqg0B7Guf9mFVtpaWyXciY2BsDsgAQ"
+    }
+    while True:
+        response = requests.get(url, headers=headers)
+        response_json = json.loads(response.text)
+        status = response_json['status']
+        if status == 'done':
+            break
+    
+    result_url = response_json['result_url']
+    
+    print(result_url)
+    return result_url
 # 語音辨識類別
 class ContinuousRecognizer:
-    def __init__(self):
+    def __init__(self, role, imgurl):
         self.speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
         self.speech_config.speech_recognition_language="zh-TW"
         self.audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
@@ -46,16 +106,13 @@ class ContinuousRecognizer:
         self.timer = None
         self.audio_path = None
         self.current_emotion = "未知"
+        self.role = role
+        self.imgurl = imgurl
 
     def start(self):
         self.recognizer.recognized.connect(self.recognized)
         self.recognizer.start_continuous_recognition()
         threading.Thread(target=self.detect_emotion).start()
-
-    def stop(self):
-        self.recognizer.stop_continuous_recognition()
-        if self.timer:
-            self.timer.cancel()
 
     def recognized(self, args):
         if args.result.reason == speechsdk.ResultReason.RecognizedSpeech:
@@ -67,19 +124,17 @@ class ContinuousRecognizer:
             self.timer.start()
 
     def respond(self, text):
-        self.stop()  # 在回應之前關閉麥克風
         try:
             if text.strip():  # 確認文本不為空
                 print(f"Responding to text: {text}")  # 確認此函數被調用
-                response_text = call_gpt(text, self.current_emotion)
+                response_text = call_gpt(text, self.role, self.current_emotion)
                 print("GPT Response: {}".format(response_text))  # 檢查 GPT 回應
-                result_url = create_did(response_text)
+                result_url = create_did(response_text, self.imgurl)
+                print(self.imgurl)
                 print("Result URL: {}".format(result_url))  # 確認 URL
                 result_queue.put(result_url)  # 將 URL 放入佇列中
-                #self.start()  # 回應完成後重新開啟麥克風
         except Exception as e:
             print(f"Error in respond: {e}")  # 打印任何錯誤
-            #self.start()  # 錯誤發生後重新開啟麥克風
     
     def detect_emotion(self):
         text_obj = {
@@ -117,77 +172,37 @@ class ContinuousRecognizer:
         cap.release()
         cv2.destroyAllWindows()
 
-# 建立 D-ID URL
-def create_did(text):
-    result_url = "https://d-id-talks-prod.s3.us-west-2.amazonaws.com/google-oauth2%7C105929470202191278895/tlk_VoQbQ1XdAQJiLGXV141Ra/1716690275935.mp4?AWSAccessKeyId=AKIA5CUMPJBIK65W6FGA&Expires=1716776680&Signature=daH6U8sDUuYagKdRFd6CTDNu2Yg%3D"
-    url = "https://api.d-id.com/talks"
 
-    payload = {
-    "script": {
-        "type": "text",
-        "subtitles": "false",
-        "provider": {
-            "type": "microsoft",
-            "voice_id": "zh-CN-XiaoxiaoNeural"
-        },
-        "ssml": "false",
-        "input": text
-    },
-    "config": {
-        "fluent": "false",
-        "pad_audio": "0.0"
-    },
-    "source_url": img_url
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-    response_json = json.loads(response.text)
-    print(response_json)
-
-    talk_id = response_json['id']
-    url = "https://api.d-id.com/talks/"+talk_id
-
-    headers = {
-        "accept": "application/json",
-    }
-    while True:
-        response = requests.get(url, headers=headers)
-        response_json = json.loads(response.text)
-        status = response_json['status']
-        if status == 'done':
-            break
-    
-    result_url = response_json['result_url']
-    print(result_url)
-    return result_url
 
 # Flask 路由
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def send_role():
+    data = request.json
+    role = data.get('role')
+    print(f"User role selected: {role}")
+    return jsonify({'status': 'success', 'role': role})
+
 @app.route("/start_recording", methods=["POST"])
 def recognize_from_microphone():
-    recognizer = ContinuousRecognizer()
+    data = request.json
+    role = data.get('role')
+    avater_img = request.get_json() 
+    image_url = avater_img['imageUrl'] 
+    print(f'image:{image_url}')
+    recognizer = ContinuousRecognizer(role, image_url)
     recognizer.start()
     return jsonify({"status": "recognition started"})
 
-@app.route("/restart_recognition", methods=["POST"])
-def restart_recognition():
-    recognizer = ContinuousRecognizer()
-    recognizer.start()  
-    return jsonify({"status": "recognition restarted"})
 
 @app.route('/get_result_url', methods=['GET'])
 def get_result_url():
     try:
         result_url = result_queue.get_nowait() 
     except queue.Empty:
-        return jsonify({"error": "小妤正在聽..."})
+        return jsonify({"error": "影片尚未準備好"})
     else:
         return jsonify({"result_url": result_url})
 
--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 var start_button = document.querySelector(".start");
 var loader = document.querySelector(".loader");
-//var img = document.querySelector(".image");
+const upload_image = document.querySelector("#upload-image");
 function startRecording() {
     axios.post('/start_recording')
         .then(function(response) {
@@ -65,3 +65,131 @@ function pollForResultUrl() {
     }
     checkForVideo(); // 開始第一次檢查
 }
+
+var start_button = document.querySelector(".start");
+var img = document.querySelector(".image");
+/*
+function startRecording() {
+    axios.post('/start_recording')
+        .then(function(response) {
+            console.log('錄音已開始，正在等待視頻...');
+            img.classList.add("active");
+            start_button.classList.add("hidden");
+            pollForResultUrl();
+        })
+        .catch(function(error) {
+            console.log('錄音開始失敗:', error);
+            document.querySelector('.result').textContent = '錄音開始失敗';
+        });
+}
+*/
+function pollForResultUrl() {
+    function checkForVideo() {
+        axios.get('/get_result_url')
+            .then(function(response) {
+                console.log(response.data.result_url);
+                if (response.data.result_url) {
+                    console.log('獲取到的視頻 URL:', response.data.result_url);
+                    var result_video = document.querySelector(".result");
+                    result_video.innerHTML = `<video width="640" height="480" controls autoplay>
+                        <source src="${response.data.result_url}" type="video/mp4">
+                        您的瀏覽器不支援 video 標籤。
+                    </video>`;
+
+                    var videoElement = result_video.querySelector("video");
+                    videoElement.addEventListener('canplaythrough', event => {
+                        videoElement.play(); // 確保影片能夠自動播放
+                    });
+
+                    videoElement.addEventListener('ended', () => {
+                        console.log('當前影片播放結束，正在請求下一段影片...');
+                        setTimeout(checkForVideo, 5000); // 當前影片播完後稍等一秒再請求新影片
+                    });
+
+                } else if (response.data.error) {
+                    console.log('錯誤訊息:', response.data.error);
+                    document.querySelector('.result').innerHTML = '<p>' + response.data.error + '</p>';
+                    setTimeout(checkForVideo, 5000); // 如果有錯誤，仍然繼續檢查
+                } else {
+                    console.log('視頻尚未準備好，再次檢查...');
+                    setTimeout(checkForVideo, 5000); // 沒有錯誤但也沒有 URL，繼續檢查
+                }
+            })
+            .catch(function(error) {
+                console.log('在獲取結果 URL 時發生錯誤:', error);
+                document.querySelector('.result').innerHTML = '<p>獲取視頻失敗</p>';
+                setTimeout(checkForVideo, 5000); // 確保即使發生錯誤也能重新嘗試
+            });
+    }
+    checkForVideo(); // 開始第一次檢查
+}
+
+let selectedRole = '';
+let imgUrl = '';
+function sendUserRole(role) {
+    selectedRole = role;
+    document.getElementById('role-name').innerText = role;
+    document.getElementById('selected-role').style.display = 'block';
+}
+
+function startRecording() {
+    if (selectedRole === '') {
+        alert('請先選擇角色');
+        return;
+    }
+
+    fetch('/start_recording', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ role: selectedRole, imageUrl:imgUrl }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        pollForResultUrl();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function uploadImage(){
+    const imageInput = document.getElementById('imageInput');
+    if (imageInput.files.length > 0) {
+        const file = imageInput.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        formData.append('type', 'image');
+        formData.append('title', 'Simple upload');
+        formData.append('description', 'This is a simple image upload in Imgur');
+
+        fetch('https://api.imgur.com/3/image', {
+            method: 'POST',
+            headers: {
+                Authorization: 'Client-ID fd0c9e6694ddbfd'
+            },
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            imgUrl = data.data.link;
+            console.log(imgUrl);
+            //showImage(imgUrl);
+        })
+        .catch((error) => {
+            alert("Error:", error);
+        });
+    } else {
+        alert('請選擇一個圖片文件。');
+    }
+}
+
+/*
+function showImage(imgUrl){
+    const img = document.querySelector("#avater-img");
+    img.src = imgUrl;
+    img.style = "display:block;";
+}
+*/
